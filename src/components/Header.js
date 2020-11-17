4bbacbecc0c1c0e3e05a5ce4cb8d8dcd25408a67
Kn0wn-Un <naikdarsh175@gmail.com>
2020-11-17 13:16:24 +0530

    header added

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/styles.css';
+
+function Header() {
+    return (
+        <div className="head">
+            <span className="page-name">CV-Create</span>
+            <a href="https://github.com/Kn0wn-Un/cv-application">GitHub</a>
+        </div>
+    );
+}
+
+export default Header;
